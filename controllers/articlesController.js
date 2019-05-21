656ad35991b64acb995082bffbdad7f4934265ea
bigalr8 <bigalr8@icloud.com>
2019-05-21 16:20:59 -0400

    prototype for handlebars with buttons and data from Mongo

--- controllers/articlesController.js
+++ controllers/articlesController.js
@@ -3,6 +3,8 @@
 var express = require("express");
 
 var router = express.Router();
+ 
+var db = require("../models");
 
 var path = require("path");
 //TO DO: Get scraped data from database
@@ -13,7 +15,7 @@ var articles = [
     article: "They won!"
     }
 ];
-
+/*
 router.get("/articles", function (req, res) {
     console.log("articles: ", articles)
     res.render("./layouts/all-articles", {
@@ -22,8 +24,33 @@ router.get("/articles", function (req, res) {
     }); 
 
  
-});
- 
+}); */
+/*
+router.get("/articles", function (req, res) {
+    //console.log("articles: ", articles)
+    db.Article.find({})
+        .then(function(dbArticle) {
+            console.log("dbArticle: ", dbArticle)
+            res.json(dbArticle);
+        })
+        .catch(function(err) {
+            res.json(err);
+        });
+   
+});*/
+
+router.get("/articles", function (req, res) {
+    //console.log("articles: ", articles)
+    db.Article.find({})
+        .then(function(dbArticle) {
+            console.log("dbArticle: ", dbArticle)
+            res.render("./layouts/all-articles", {
+                media: dbArticle,
+                source: "Philly.com"
+            }); 
+        });    
+});     
+
 var article = require("../models/index.js");
 
 module.exports = router;
